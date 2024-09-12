69e1d4f2c05963995570526e01e79331925792b3
Flopylepk <florencia.torres@davinci.edu.ar>
2024-09-12 10:12:49 -0300

    operaciones

--- src/main.java
+++ src/main.java
@@ -10,9 +10,30 @@ public class main {
 		String patente=validar1("ingrese cual es la patente");
 		
 		VTV auto= new VTV (dueño, marca, modelo, patente);
-		JOptionPane.showMessageDialog(null, auto);
+		JOptionPane.showMessageDialog(null, "ok, ya registramos al auto, sugamos");
+		double motor=validar2("Ingrese el porsentaje que dio el motor después de las pruebas");
+		double frenos=validar2("Ingrese el porsentaje que dioeron los frenos después de las pruebas");
+		double suspencion=validar2("Ingrese el porsentaje que dio la suspencion después de las pruebas");
+		VTV prueba=new VTV(motor, frenos, suspencion);
+		JOptionPane.showMessageDialog(null, "ok ahora verifiquemos si paso o no");
+		JOptionPane.showMessageDialog(null, "Realizando operaciones...");
 		
 		
+		String opcion;
+		String lista="";
+		double porsentajegeneral= (motor+frenos+suspencion)/3;
+		if (porsentajegeneral<60) {
+			opcion="no";
+			if (motor<50) {
+				lista= lista+"el motor fallo \n";
+			} 
+			if (frenos<50) {
+				lista=lista+"el freno fallo \n";
+			}
+			
+		} else {
+			opcion="si";
+		}
 		
 		
 		
@@ -30,7 +51,26 @@ public class main {
 		return a;
 		
 	}
-	
+	public static int validar2 (String mensaje) {
+		boolean flag=true;
+		String x;
+		do {
+		x=JOptionPane.showInputDialog(mensaje);
+		while (x.isEmpty()) {
+			 x=JOptionPane.showInputDialog("Error: "+ mensaje);
+		}
+		for (int i = 0; i < x.length(); i++) {
+			if (!Character.isDigit(x.charAt(i))) {
+				JOptionPane.showMessageDialog(null, "usted no completo un número");
+				flag=false;
+				break;
+			}else {
+				JOptionPane.showMessageDialog(null, "Su numero es correcto puede continuar");
+			}
+		}
+		} while (!flag);
+		return Integer.parseInt(x);
+	}
 	
 
 }
