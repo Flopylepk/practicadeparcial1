41242cb72a663bb03ea2401b296a4c06f723930d
Flopylepk <florencia.torres@davinci.edu.ar>
2024-09-12 09:26:19 -0300

    5

--- src/main.java
+++ src/main.java
@@ -1,9 +1,10 @@
+import javax.swing.JOptionPane;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+JOptionPane.showMessageDialog(null, "hola");
 	}
 
 }
