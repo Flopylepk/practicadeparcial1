52e71ad054529897330c481876ed0b04a09693e6
Flopylepk <florencia.torres@davinci.edu.ar>
2024-09-14 12:21:12 -0300

    creación de atributos

--- src/practica2/Construccion.java
+++ src/practica2/Construccion.java
@@ -1,5 +1,16 @@
 package practica2;
 
+import java.time.LocalDate;
+
 public class Construccion {
+private String proyecto;
+private String etapas;
+
+
+public void Construcción(String a, String b) {
+	this.proyecto=a;
+	this.etapas=b;
+}
+
 
 }
